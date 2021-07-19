e474e53b1f8b77aa15fe8a4ef94bf245829e1e3c
Spirit-Tiger <sprttiger@gmail.com>
2021-07-19 13:31:34 +0300

    Starting with styled components

--- capture/src/App.js
+++ capture/src/App.js
@@ -1,10 +1,10 @@
 import AboutUs from "./pages/AboutUs";
-
+import  GlobalStyle from "./components/GlobalStyle";
 
 function App() {
   return (
     <div className="App">
-      <h1>Film</h1>
+      <GlobalStyle />
       <AboutUs/>
     </div>
   );
--- capture/src/components/AboutSection.jsx
+++ capture/src/components/AboutSection.jsx
@@ -1,28 +1,63 @@
 import React from "react";
 import home1 from "../img/home1.png";
 
+import styled from "styled-components";
+
 const AboutSection = () => {
         return(
-            <div>
-                <div className="description">
+            <About>
+                <Description>
                     <div className="title">
-                        <div className="hide">
+                        <Hide>
                             <h2>We work to make</h2>
-                        </div>
-                        <div className="hide">
+                        </Hide>
+                        <Hide className="hide">
                             <h2>your <span> dreams</span> come </h2>
-                        </div>
-                        <div className="hide">
+                        </Hide>
+                        <Hide className="hide">
                             <h2>true.</h2>
-                        </div>
+                        </Hide>
                     </div>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam possimus corporis itaque quas, perspiciatis, distinctio odio minus repellendus qui nisi voluptatum est ad enim? Harum eius sapiente fugit quae excepturi.
                     Nesciunt impedit earum doloremque quos libero illo assumenda repellat optio, ex voluptates consequuntur porro tempore repellendus inventore unde accusamus deleniti facilis atque soluta tenetur, dolore similique. Debitis totam alias temporibus.</p>
                     <button>Contact Us</button>
-                </div>
-                <img src={home1} alt="home1" />
-            </div>
-        )
-}
+                </Description>
+                <Image>
+                 <img src={home1} alt="home1" />
+                 </Image>
+            </About>
+        );
+};
+
+const About = styled.div`
+    min-height:90vh;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5rem 10rem;
+    color: white;
+`;
+
+const Description = styled.div`
+    flex: 1;
+    padding-right: 5rem;
+    h2{
+        font-weight: lighter;
+    }
+`;
+
+const Image = styled.div`
+    flex: 1;
+    overflow: hidden;
+    img{
+        width: 100%;
+        height: 90vh;
+        object-fit: cover;
+    }
+`;
+
+const Hide = styled.div`
+    overflow: hidden;
+`;
 
 export default AboutSection;
